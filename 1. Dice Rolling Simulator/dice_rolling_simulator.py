823ea7295d74bf894b6ad713388017abccf30fb5
AlexanderDBell <alexander.bell17601@gmail.com>
2024-01-08 14:36:13 +0000

    Added Dice Rolling Simulator and Guess the Number

---
+++ 1. Dice Rolling Simulator/dice_rolling_simulator.py
@@ -0,0 +1,25 @@
+import random
+
+class Dice:
+    def __init__(self, min: int = 1, max: int = 6) -> None:
+        self.min = min
+        self.max = max
+
+    def roll(self):
+        while True:
+            result = random.randint(self.min, self.max)
+            print(f'Your roll is {result}')
+            while True:
+                reroll = input('Would you like to roll again (y/n)? ')
+                reroll = reroll.lower()
+                if reroll not in ('y', 'n'):
+                    print('Please enter y/n.')
+                    continue
+                break
+            if reroll.lower() == 'y':
+                continue
+            elif reroll.lower() == 'n':
+                break
+
+d6 = Dice()
+d6.roll()
---
+++ 2. Guess the Number/guess_the_number.py
@@ -0,0 +1,27 @@
+import random
+
+class GuessingGame:
+    def __init__(self, min: int = 1, max: int = 100) -> None:
+        self.min = min
+        self.max = max
+    
+    def run(self) -> None:
+        print(f'Generating a number from {self.min} to {self.max}.')
+        number = random.randint(self.min, self.max)
+        while True:
+            try:
+                guess = input('What is the number? ')
+                guess = int(guess)
+            except ValueError:
+                print('Please enter an integer.')
+                continue
+            if guess < number:
+                print('Too low.')
+            elif guess > number:
+                print('Too high.')
+            elif guess == number:
+                print('You Win!')
+                break
+
+game = GuessingGame()
+game.run()
